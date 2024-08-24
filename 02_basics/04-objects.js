ef3952cdf556d83f96bf64e1a45c86292bf29557
ShabanHashim381 <shabantararr381@gmail.com>
2024-08-25 03:12:29 +0500

    04-objects.js

--- 02_basics/04-objects.js
+++ 02_basics/04-objects.js
@@ -44,12 +44,39 @@ const users = [
         email: "s@gmail.com"
     }
 ]
+users[1].email 
 
-users[1].email
-console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+const course = {
+    coursename: "js n urdu",
+    price: "999",
+    courseInstructor: "shaban"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+// {
+//     "name": "shaban",
+//     "coursename":"js in urdu",
+//     "price": "free"
+// }
+[
+    {},
+    {},
+    {}
+]
