a56b3615e65e62c1466dd1791239e455c4653dca
ShabanHashim381 <shabantararr381@gmail.com>
2024-08-29 22:41:45 +0500

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if
+const isUserloggedIn = true
+const temprature = 41
+
+// if (temprature === 41){
+//    console.log("less than 50");
+// } else{
+//     console.log("temprature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, ===
+
+// const score = 200
+
+// if (score > 100) {
+//     var power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+const Balance = 1000 
+
+// if (Balance) console.log("test"),console.log("test2");
+
+// if (Balance < 500) {
+//     console.log("less than 500");
+
+// } else if (Balance < 750 ) {
+//    console.log("less then 750");
+
+// } else if (Balance < 900) {
+//     console.log("less than 750");
+
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userloggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+   console.log("User logged in");
+}
